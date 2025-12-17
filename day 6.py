1e095bb46e8995d7b52a774ccf09d374a749c3f2
thas117 <thaslimajaleel8393@gmail.com>
2025-12-17 11:20:34 +0530

    DATA SCIENCE

--- day 6.py
+++ day 6.py
@@ -1,24 +1,46 @@
-class person:
-    x=10
-    y=20
-    tup=(1,2,3)
-
-
-
-
+#class Person:
+#     x=10
+#     y=20
+#     tup=(1,2,3)
+#     c=[2,4,5]
+#     d={100,200,300}
+#     def func(self):
+#         print("this is the my class person")
+#         p1=person()
+#         p1.func()
+#     print(p1.x)
+#     print(p1.y)
+#     print(p1.tup)
+#     print(p1.c)
+#     print(p1.d)
 
 
 
 class Engine5kcc:
-    x=5
-    def Onroad(self):
-        print("500km/hr")
+    def __init__(self, kmph,limit):
+        self.kmph = kmph
+        self.limit = limit
 
-bmw = Engine5kcc()
-audi = Engine5kcc()
-ferrari = Engine5kcc()
+    def Onroad(self, weight):
+        print(self.kmph-weight)
+        print("Limit is", self.limit)
 
-bmw.Onroad()
-print(bmw.x)
-audi.Onroad()
-ferrari.Onroad()
+bmw = Engine5kcc(200,450)
+audi = Engine5kcc(100,300)
+
+
+bmw.Onroad(20)
+audi.Onroad(10)
+
+
+
+# class Calculation:
+#     def add(self,a,b):
+#         return a+b
+#     def sub(self,a,b):
+#         return a-b
+# cal = Calculation()
+# print(cal.add(20,10))
+# print(cal.sub(20,10))
+
+
