2c98acf604550b282a19656b6878d6e6302c62d4
thas117 <thaslimajaleel8393@gmail.com>
2025-12-17 15:14:39 +0530

    DATA SCIENCE

--- day 6.py
+++ day 6.py
@@ -16,21 +16,20 @@
 
 
 
-class Engine5kcc:
-    def __init__(self, kmph,limit):
-        self.kmph = kmph
-        self.limit = limit
+# class Engine5kcc:
+#     def __init__(self,kmph,limit):
+#         self.kmph = kmph =  100
+#         self.limit = limit = 200
 
-    def Onroad(self, weight):
-        print(self.kmph-weight)
-        print("Limit is", self.limit)
+#     def Onroad(self, weight):
+#         print("Limit is", self.limit)
+#         print("self.kmph-weight")
+# bmw = Engine5kcc(200,300)
+# audi = Engine5kcc(100,300)
 
-bmw = Engine5kcc(200,450)
-audi = Engine5kcc(100,300)
 
-
-bmw.Onroad(20)
-audi.Onroad(10)
+# bmw.Onroad(20)
+# audi.Onroad(10)
 
 
 
@@ -43,4 +42,37 @@ audi.Onroad(10)
 # print(cal.add(20,10))
 # print(cal.sub(20,10))
 
+# class math:
+#     formula = "(a+b)^2"
 
+# class chemistry(math):
+#     pass
+# obj = chemistry()
+# print(obj.formula)
+
+# class math:
+#     formula = "(a+b)^2"
+
+# class chemistry(math):
+#     x=17
+# class bio(chemistry):
+#     pass
+# obj = bio()
+# print(obj.formula)
+# print(obj.x)
+
+
+class xyz:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def add(self):
+        return self.a+self.b
+class abc(xyz):
+        def __init__(self,a,b):
+            a+=10
+            b+=20
+            super().__init__(a,b)
+
+a = abc(10,20)
+print(a.add())
